d9d214ae0d4a2ad022d9fd7f9fd9de312c49581c
White-Wtatt <yang013023@163.com>
2026-01-15 22:27:01 +0800

    vault backup: 2026-01-15 22:27:01

---
+++ C++ 《C++Primer Plus Six》/4 复合类型/assets/4.2 字符串/instr1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ArSize = 20;
+
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name :\n";
+    cin >> name;
+
+    cout << "Enter your favorite dessert:\n";
+    cin >> dessert;
+
+    cout << "I have some delicious " << dessert << "for you"
+    << name;
+
+    return 0;
+}
