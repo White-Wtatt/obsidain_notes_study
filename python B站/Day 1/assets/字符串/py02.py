1bfe984bbf9a7ce65a08425a7264bb68c75a6ab0
White-Wtatt <yang013023@163.com>
2026-03-11 14:44:51 +0800

    vault backup: 2026-03-11 14:44:51

--- python B站/Day 1/assets/字符串/py02.py
+++
@@ -1,5 +0,0 @@
-a = 'hello'
-print(type(a))
-a1 = a.encode()
-print(a1)
-print(type(a1))
--- python B站/Day 1/assets/字符串/py03.py
+++
@@ -1,7 +0,0 @@
-print(10 + 20)
-print('10' + '20')
-
-a1 = "你好"
-a2 = "世界"
-print(a1 + a2)
-print(a1, a2,sep='')
